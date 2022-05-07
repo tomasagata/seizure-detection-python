167f086c1cc4a82a3d276b4a946fad9ed770aa74
tommudano <38632202+tommudano@users.noreply.github.com>
2022-05-07 16:43:10 -0300

    Update lectura.py

--- lectura.py
+++ lectura.py
@@ -22,13 +22,11 @@ def readSignals():
     lstColors = ['r', 'g', 'b', 'c', 'm', 'y', 'k']
     f = pyedflib.EdfReader("./chb01/chb01_03.edf.seizureFile.edf")
     n=f.signals_in_file
-    denoiseFourier(f.readSignal(0))
     figure, axs = plt.subplots(n)
     signal_labels = f.getSignalLabels()
-    # return f.readSignal(i), signal_labels[i]
     for i in range(0, n):
         axs[i].set_title(signal_labels[i], x=-0.075, y=-0.1)
-        axs[i].plot(f.readSignal(i), lstColors[i % len(lstColors)])
+        axs[i].plot(denoiseFourier(f.readSignal(i)), lstColors[i % len(lstColors)])
     plt.show()
 
 # readSignals()
